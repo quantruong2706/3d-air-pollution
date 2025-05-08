c472913eac1f25302b7f2f9e3b3156d02af9a9ae
truongnguyen <truongilv3000@gmail.com>
2025-05-09 00:12:18 +0700

    chore: fix lint in filterStore.ts

--- src/stores/filterStore.ts
+++ src/stores/filterStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
-import { PollutionType } from './pollutionDataStore';
 import { COLORS } from '@/styles/themes/colors';
+import { PollutionType } from '@/types/3dVisualization';
 
 export interface Filter {
   label: string;
